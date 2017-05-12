5034102a30e2a8e73bf84c76a6f26d71dd496a03
vincent youmans <vinceyoumans@gmail.com>
2017-05-12 09:58:27 -0400

    Works... gets Token from IONIC...  not test with PUSH message yet

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +7,39 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
+     setAccount(account){
+        // let alert = this.alertCtrl.create({
+        //   title:"TEXTBOX SAYS",
+        //   subTitle:account,
+        //   buttons:["ok"]
+        // });
+        
+        // alert.present();
+
+            console.log('VY***********************************************');
+            console.log('Account: ' + account  );
+                    // alert('account: ' + account );
+                    window.localStorage.setItem("account", account);
+                    // alert('++account: ' + window.localStorage.getItem("account") );
+            console.log('VY***********************************************');  
+
+        let alert = this.alertCtrl.create({
+          title:"DEVICE TOKEN",
+          subTitle: window.localStorage.getItem("deviceToken"),
+          buttons:["ok"]
+        });
+        
+        alert.present();
+
+      alert = this.alertCtrl.create({
+          title:"Account",
+          subTitle: window.localStorage.getItem("account"),
+          buttons:["ok"]
+        });
+        
+        alert.present();
+    }
 
 }
